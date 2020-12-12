663ee1a440a6fbbd71566f0868a7b3d85cda35ae
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2020-12-12 20:27:32 +0530

    Add starter playground

---
+++ Starter.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+let input = """
+""".components(separatedBy: .newlines)
+
+print(input)
+
+func partOne() -> Int {
+    return 0
+}
+
+func partTwo() -> Int {
+    return 0
+}
+
+print("Part One answer is: \(partOne())")
+print("Part Two answer is: \(partTwo())")
