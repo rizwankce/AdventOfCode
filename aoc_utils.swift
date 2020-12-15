ca2dd0a138fc3b399aaede67a2c47112d69fb121
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2020-12-15 16:53:18 +0530

    add extensions to utils

--- aoc_utils.swift
+++ aoc_utils.swift
@@ -24,6 +24,30 @@ struct Point: CustomStringConvertible, Equatable, Hashable {
     }
 }
 
+struct Tile: CustomStringConvertible, Equatable, Hashable {
+    var row: Int
+    var col: Int
+
+    var description: String {
+        get {
+            return "Row: \(self.row) Col: \(self.col)"
+        }
+    }
+
+    func adjacent() -> [Point] {
+        return [
+            Tile.init(x: x-1, y: y),
+            Tile.init(x: x+1, y: y),
+            Tile.init(x: x, y: y-1),
+            Tile.init(x: x, y: y+1),
+            Tile.init(x: x-1, y: y-1),
+            Tile.init(x: x+1, y: y+1),
+            Tile.init(x: x+1, y: y-1),
+            Tile.init(x: x-1, y: y+1)
+        ]
+    }
+}
+
 var grid: [Point: String] = [:]
 var maxX: Int = input.count
 var maxY: Int = input[0].count
@@ -56,3 +80,63 @@ public func ==<K, L: Hashable, R: Hashable>(lhs: [K: L], rhs: [K: R] ) -> Bool {
     (lhs as NSDictionary).isEqual(to: rhs)
 }
 
+// extensions
+extension Int {
+    var bits: [Int] {
+        var array: [Int] = []
+        var copy = self
+        while copy > 0 {
+            array.append(copy % 2)
+            copy /= 2
+        }
+        return array.reversed()
+    }
+}
+
+extension Array where Element == Int {
+    var intValue: Int {
+        var value = 0
+        for element in self {
+            value *= 2
+            value += element
+        }
+        return value
+    }
+}
+
+extension Array where Element: Hashable {
+    var uniques: Array {
+        var buffer = Array()
+        var added = Set<Element>()
+        for elem in self {
+            if !added.contains(elem) {
+                buffer.append(elem)
+                added.insert(elem)
+            }
+        }
+        return buffer
+    }
+}
+
+// combinators
+
+func combos<T>(elements: ArraySlice<T>, k: Int) -> [[T]] {
+    if k == 0 {
+        return [[]]
+    }
+
+    guard let first = elements.first else {
+        return []
+    }
+
+    let head = [first]
+    let subcombos = combos(elements: elements, k: k - 1)
+    var ret = subcombos.map { head + $0 }
+    ret += combos(elements: elements.dropFirst(), k: k)
+
+    return ret
+}
+
+func combos<T>(elements: Array<T>, k: Int) -> [[T]] {
+    return combos(elements: ArraySlice(elements), k: k)
+}
