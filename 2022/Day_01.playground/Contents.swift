81b610a0e7f21d52af4c1af58fca30217f742305
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2022-12-01 21:02:17 +0530

    Add 2022 day 1 solution

---
+++ 2022/Day_01.playground/Contents.swift
@@ -0,0 +1,57 @@
+import Cocoa
+
+let input = load(.input, runFromTerminal:true).trimmingCharacters(in: .newlines)
+
+let cals = input.components(separatedBy: "\n\n")
+let total = cals
+    .map { $0.components(separatedBy: .newlines).compactMap { Int($0) }.reduce(0, +) }
+
+func partOne() -> Int {
+    total.max()!
+}
+
+func partTwo() -> Int {
+    total.sorted(by: >)[0..<3].reduce(0, +)
+}
+
+print("Part One answer is: \(partOne())")
+print("Part Two answer is: \(partTwo())")
+
+enum PuzzleInput: String {
+    case input = "input"
+    case test  = "test_input"
+}
+
+func load(_ input: PuzzleInput, runFromTerminal: Bool = false) -> String {
+    switch input {
+    case .input:
+        return load(file: "input", runFromTerminal)
+    default:
+        return load(file: "test_input", runFromTerminal)
+    }
+}
+
+func load(file name: String, _ runFromTerminal: Bool = false) -> String {
+
+    if runFromTerminal {
+        let projectURL = URL(fileURLWithPath: #file)
+                .deletingLastPathComponent()
+                .appendingPathComponent("Resources")
+        let fileURL = projectURL.appendingPathComponent(name+".txt")
+        guard let content = try? String(contentsOf: fileURL, encoding: .utf8) else {
+            fatalError("Cannot convert file contents to string for file :\(name)")
+        }
+
+        return content
+    }
+
+    guard let url = Bundle.main.url(forResource: name, withExtension: "txt") else {
+        fatalError("Cannot load file with name :\(name)")
+    }
+
+    guard let content = try? String(contentsOf: url, encoding: .utf8) else {
+        fatalError("Cannot convert file contents to string for file :\(name)")
+    }
+
+    return content
+}
