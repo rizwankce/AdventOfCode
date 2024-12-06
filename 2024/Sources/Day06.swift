4bbb8f08b66e98abd586ef640884eb15e649a45c
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-12-06 17:26:33 +0100

    Add day 6 solutions

--- 2024/Sources/Day06.swift
+++ 2024/Sources/Day06.swift
@@ -1,17 +1,96 @@
 import Algorithms
 
 struct Day06: AdventDay {
+	typealias Grid = Grid2d<Character>
+
 	var data: String
 
-	var input: [String] {
-		data.lines
+	var grid: Grid {
+		Grid(data.lines)
 	}
 
 	func part1() -> Any {
-		return 0
+		var start = Point(x: 0, y: 0)
+		outer: for r in (0..<grid.rowCount) {
+			for c in (0..<grid.colCount) {
+				let p = Point(x: c, y: r)
+				if grid[p] == "^" {
+					start = p
+					break outer
+				}
+			}
+		}
+
+		var visited: [Point] = [start]
+		var direction: Direction = .north
+		while true {
+			let newP = visited.last!.moved(1, direction)
+			guard grid[newP] != nil else {
+				break
+			}
+			if grid[newP] == "#" {
+				direction.turn(90, .right)
+			}
+			else {
+				visited.append(newP)
+			}
+		}
+
+		return visited.uniques.count
+	}
+
+	func isLoop(_ g: Grid, start: Point, block: Point) -> Bool {
+		g[block] = "%"
+		var visited: [Point] = [start]
+		var path: Set<Point> = [start]
+		var direction: Direction = .north
+		var isLoop = false
+		while true {
+			let newP = visited.last!.moved(1, direction)
+			guard g[newP] != nil else {
+				break
+			}
+
+			if visited.count / 2 == path.count {
+				isLoop = true
+				break
+			}
+
+			if g[newP] == "#" || g[newP] == "%" {
+				direction.turn(90, .right)
+			}
+			else {
+				visited.append(newP)
+				path.insert(newP)
+			}
+		}
+		return isLoop
+	}
+
+	func printG(_ points: [Point]) {
+		let g = grid
+		points.forEach { g[$0] = "X" }
+		print(g)
 	}
 
 	func part2() -> Any {
-		return 0
+		var start = Point(x: 0, y: 0)
+		var toSearch: [Point] = []
+		for r in (0..<grid.rowCount) {
+			for c in (0..<grid.colCount) {
+				let p = Point(x: c, y: r)
+				if grid[p] == "^" {
+					start = p
+				}
+				if grid[p] == "." {
+					toSearch.append(p)
+				}
+			}
+		}
+
+		return
+			toSearch
+			.filter { isLoop(grid, start: start, block: $0) }
+			.count
 	}
 }
--- 2024/Tests/Day06.swift
+++ 2024/Tests/Day06.swift
@@ -4,16 +4,25 @@ import Testing
 
 struct Day06Tests {
 	let testData = """
-
+		....#.....
+		.........#
+		..........
+		..#.......
+		.......#..
+		..........
+		.#..^.....
+		........#.
+		#.........
+		......#...
 		"""
 
 	@Test func testPart1() async throws {
 		let challenge = Day06(data: testData)
-		#expect(String(describing: challenge.part1()) == "0")
+		#expect(String(describing: challenge.part1()) == "41")
 	}
 
 	@Test func testPart2() async throws {
 		let challenge = Day06(data: testData)
-		#expect(String(describing: challenge.part2()) == "0")
+		#expect(String(describing: challenge.part2()) == "6")
 	}
 }
