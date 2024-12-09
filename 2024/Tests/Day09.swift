e782edb11de531b82ed82975a4a066f77f3c806f
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-12-09 17:05:34 +0100

    Add day 9 solution

--- 2024/Tests/Day09.swift
+++ 2024/Tests/Day09.swift
@@ -3,17 +3,15 @@ import Testing
 @testable import AdventOfCode
 
 struct Day09Tests {
-	let testData = """
-
-		"""
+	let testData = "2333133121414131402"
 
 	@Test func testPart1() async throws {
 		let challenge = Day09(data: testData)
-		#expect(String(describing: challenge.part1()) == "0")
+		#expect(String(describing: challenge.part1()) == "1928")
 	}
 
 	@Test func testPart2() async throws {
 		let challenge = Day09(data: testData)
-		#expect(String(describing: challenge.part2()) == "0")
+		#expect(String(describing: challenge.part2()) == "2858")
 	}
 }
