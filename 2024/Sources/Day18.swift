f7cf4f84bc0f8d0026e9400e788fc9824fbc4081
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-12-18 14:32:06 +0100

    Add day 18 solution

--- 2024/Sources/Day18.swift
+++ 2024/Sources/Day18.swift
@@ -3,15 +3,88 @@ import Algorithms
 struct Day18: AdventDay {
   var data: String
 
-  var input: [String] {
-    data.lines
+  var input: [Point] {
+    data.lines.map { line in
+      let com = line.components(separatedBy: ",")
+      return Point(x: Int(com[0])!, y: Int(com[1])!)
+    }
+  }
+
+  func simulate(_ count: Int, _ rowCount: Int, _ colCount: Int) -> [Point] {
+    let points = input[0...count - 1]
+
+    func printGrid(_ seen: Set<Point> = []) {
+      var str = ""
+      for r in (0..<rowCount) {
+        for c in (0..<colCount) {
+          let p = Point(x: c, y: r)
+          if points.contains(p) {
+            str += "#"
+          } else if seen.contains(p) {
+            str += "O"
+          } else {
+            str += "."
+          }
+        }
+        str += "\n"
+      }
+      print(str)
+    }
+
+    func isValid(_ point: Point) -> Bool {
+      0 <= point.x && point.x < colCount && 0 <= point.y && point.y < rowCount
+    }
+
+    struct State: Hashable {
+      let point: Point
+      let path: [Point]
+    }
+
+    let start = State(point: Point.start, path: [])
+    let end = Point(x: rowCount - 1, y: colCount - 1)
+    var queue: [State] = [start]
+    var visited: Set<Point> = []
+    var path: [Point] = []
+    while !queue.isEmpty {
+      let current = queue.removeFirst()
+      if visited.contains(current.point) {
+        continue
+      }
+      visited.insert(current.point)
+
+      if current.point == end {
+        path = current.path
+        break
+      }
+
+      for neighbor in current.point.adjacent() {
+        if !points.contains(neighbor) && isValid(neighbor) {
+          queue.append(State(point: neighbor, path: current.path + [neighbor]))
+        }
+      }
+    }
+    return path
+  }
+
+  func simulate(_ gridLength: Int) -> String {
+    var ans = Point.start
+    for i in (1...input.count - 1) {
+      let path = simulate(i, gridLength, gridLength)
+      if path.count == 0 {
+        let points = input[0...i - 1]
+        ans = points.last!
+        break
+      }
+    }
+
+    return "\(ans.x),\(ans.y)"
   }
 
   func part1() -> Any {
-    return 0
+    simulate(1024, 71, 71).count
   }
 
   func part2() -> Any {
-    return 0
+    simulate(71)
   }
 }
--- 2024/Tests/Day18.swift
+++ 2024/Tests/Day18.swift
@@ -4,16 +4,40 @@ import Testing
 
 struct Day18Tests {
   let testData = """
-
+    5,4
+    4,2
+    4,5
+    3,0
+    2,1
+    6,3
+    2,4
+    1,5
+    0,6
+    3,3
+    2,6
+    5,1
+    1,2
+    5,5
+    2,5
+    6,5
+    1,4
+    0,4
+    6,4
+    1,1
+    6,1
+    1,0
+    0,5
+    1,6
+    2,0
     """
 
   @Test func testPart1() async throws {
     let challenge = Day18(data: testData)
-    #expect(String(describing: challenge.part1()) == "0")
+    #expect(String(describing: challenge.simulate(12, 7, 7).count) == "22")
   }
 
   @Test func testPart2() async throws {
     let challenge = Day18(data: testData)
-    #expect(String(describing: challenge.part2()) == "0")
+    #expect(String(describing: challenge.simulate(7)) == "6,1")
   }
 }
