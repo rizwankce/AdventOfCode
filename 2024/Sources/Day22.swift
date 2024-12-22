f5f96ba81169db51fe34e5793606b2652cc50e5b
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-12-22 11:48:55 +0100

    Add day 22 solution

--- 2024/Sources/Day22.swift
+++ 2024/Sources/Day22.swift
@@ -1,17 +1,96 @@
 import Algorithms
+import Foundation
 
 struct Day22: AdventDay {
   var data: String
 
-  var input: [String] {
-    data.lines
+  var input: [Int] {
+    data.lines.compactMap { Int($0) }
+  }
+
+  func evovles(_ number: Int) -> Int {
+    var number = number
+    var result = number * 64
+    number = number ^ result
+    number = number % 16_777_216
+    //print(number)
+    let dresult = Double(number) / Double(32)
+    result = Int(dresult.rounded(.down))
+    number = number ^ result
+    number = number % 16_777_216
+    //print(number)
+
+    result = number * 2048
+    number = number ^ result
+    number = number % 16_777_216
+    //print(number)
+
+    return number
+  }
+
+  func evolve(_ number: Int, _ steps: Int) -> Int {
+    var number = number
+    for _ in 1...steps {
+      number = evovles(number)
+    }
+    return number
+  }
+
+  func evolve2(_ number: Int, _ steps: Int) -> [Int] {
+    var number = number
+    var result: [Int] = [number % 10]
+    for _ in 1...steps {
+      number = evovles(number)
+      result.append(number % 10)
+    }
+    return result
   }
 
   func part1() -> Any {
-    return 0
+    input.map { evolve($0, 2000) }.reduce(0, +)
   }
 
   func part2() -> Any {
-    return 0
+    struct State: Hashable, CustomStringConvertible {
+      var description: String {
+        "\(a),\(b),\(c),\(d)"
+      }
+      let a: Int
+      let b: Int
+      let c: Int
+      let d: Int
+
+      init(_ diff: [Int]) {
+        assert(diff.count == 4)
+        a = diff[0]
+        b = diff[1]
+        c = diff[2]
+        d = diff[3]
+      }
+    }
+
+    var score: [State: Int] = [:]
+    for number in input {
+      let result = evolve2(number, 2000)
+      var diffs: [Int] = []
+
+      for (i, j) in result.adjacentPairs() {
+        diffs.append(j - i)
+      }
+      var search: [State: Int] = [:]
+      for i in 0..<diffs.count - 3 {
+        let state = State(Array(diffs[i...i + 3]))
+        if !search.keys.contains(state) {
+          search[state] = result[i + 4]
+        }
+      }
+
+      for (k, v) in search {
+        score[k, default: 0] += v
+      }
+
+    }
+
+    return score.values.max()!
   }
 }
--- 2024/Tests/Day22.swift
+++ 2024/Tests/Day22.swift
@@ -3,17 +3,25 @@ import Testing
 @testable import AdventOfCode
 
 struct Day22Tests {
-  let testData = """
-
-    """
-
   @Test func testPart1() async throws {
-    let challenge = Day22(data: testData)
-    #expect(String(describing: challenge.part1()) == "0")
+    let challenge = Day22(
+      data: """
+        1
+        10
+        100
+        2024
+        """)
+    #expect(String(describing: challenge.part1()) == "37327623")
   }
 
   @Test func testPart2() async throws {
-    let challenge = Day22(data: testData)
-    #expect(String(describing: challenge.part2()) == "0")
+    let challenge = Day22(
+      data: """
+        1
+        2
+        3
+        2024
+        """)
+    #expect(String(describing: challenge.part2()) == "23")
   }
 }
